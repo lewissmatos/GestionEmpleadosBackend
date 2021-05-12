8a407c41829d384cdcbb467340845356ce06838f
Lewis Matos <lewiismb1@example.com>
2021-05-12 09:33:01 +0200

    controllers y endpoints de cargos agregados

--- src/controllers/cargo.controller.js
+++ src/controllers/cargo.controller.js
@@ -1,1 +1,47 @@
 const Cargo =  require ('../models/cargo.model')
+
+exports.getAllCargos = async(req , res)=>{
+
+    try {
+        
+        const cargos = await Cargo.find()
+
+        res.status(200).json({ok:true, data: cargos})
+
+    } catch (error) {
+        res.status(500).json({ok:false, msg: error})
+        console.log(error);
+    }
+}
+
+exports.getCargosByArea = async(req, res)=>{
+
+    const { area }= req.params
+
+    try {
+
+        const cargos = await Cargo.find({area})
+
+        res.status(200).json({ok:true, data: cargos})
+        
+    } catch (error) {
+        res.status(500).json({ok:false, msg: error})
+        console.log(error);
+    }
+
+}
+
+exports.createCargo = async(req, res)=>{
+    try {
+        
+        const cargo = new Cargo(req.body)
+
+        await cargo.save()
+
+        res.status(200).json({ok: true, data:cargo})
+
+    } catch (error) {
+        res.status(500).json({ok:false, msg: error})
+        console.log(error);
+    }
+}
--- src/routes/cargo.routes.js
+++ src/routes/cargo.routes.js
@@ -2,8 +2,12 @@ const Router = require('express')
 
 const cargoCtrl = require('../controllers/cargo.controller')
 
-
-
 let router = Router()
+
+router.get('/', cargoCtrl.getAllCargos)
+
+router.get('/:area', cargoCtrl.getCargosByArea)
+
+router.post('/', cargoCtrl.createCargo)
 
 module.exports = router
